b7e0a9e9c7d92bccaf26ec95a7ac075bba24d1ff
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-25 16:33:02 +0530

    [SP]Added: Invoice Generator Returns Invoice Summary As A Whole

--- src/test/java/Cab/Invoice/Generator/CabInvoiceTest.java
+++ src/test/java/Cab/Invoice/Generator/CabInvoiceTest.java
@@ -9,10 +9,18 @@ import junit.framework.Assert;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class CabInvoiceTest {
+	CabInvoice cabInvoice = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		cabInvoice = new CabInvoice();
+	}
+	
 	@Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-    	CabInvoice cabInvoice = new CabInvoice();
     	double distance = 2.0;
     	int time = 5;
     	double fare = cabInvoice.calculateFare(distance, time);
@@ -20,11 +28,11 @@ public class CabInvoiceTest {
     }
 	
 	@Test
-    public void givenMultipleRide_ShouldReturnTotalFare() {
-    	CabInvoice cabInvoice = new CabInvoice();
+    public void givenMultipleRide_ShouldReturnInvoiceSummary() {
     	Ride rides[] = {new Ride(2.0, 5),
     				   new Ride(0.1, 1)};
-    	double fare = cabInvoice.calculateFare(rides);
-    	Assert.assertEquals(30, fare, 0.0);
+    	InvoiceSummary invoiceSummary = cabInvoice.calculateFare(rides);
+    	InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+    	Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
 }
